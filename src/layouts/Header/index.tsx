d0781d690bf06f4089c9571e496d2c3685f9905f
Yang-Heejeong <yanghj990@gmail.com>
2023-08-29 12:11:04 +0900

    messe

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,13 +1,16 @@
-import React, { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 import './style.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AUTH_PATH ,MAIN_PATH, BOARD_WRITE_PATH, SEARCH_PATH, BOARD_DETAIL_PATH, USER_PATH, BOARD_UPDATE_PATH } from 'constant';
+import { useCookies } from 'react-cookie';
 
 //          component: 헤더 컴포넌트          //
 export default function Header() {
 
   //          state: path name 상태          //
   const { pathname } = useLocation();
+  //          state: cookie 상태          //
+  const [cookies, setCookies] = useCookies();
 
   //          variable: 인증 페이지 논리 변수          //
   const isAuthPage =  pathname === AUTH_PATH;
@@ -70,6 +73,25 @@ export default function Header() {
 
   }
 
+  //          component: 로그인 상태에 따라 로그인 혹은 마이페이지 버튼 컴포넌트          //
+  const LoginMyPageButton = () => {
+
+    if (cookies.email)
+    return (
+      <div className='mypage-button'>마이페이지</div>
+    )
+    
+    return (
+      <div className='login-button'>로그인</div>
+    )
+    
+  }
+
+  //          effect: 마운트시에만 실행될 함수          //
+  useEffect(() => {
+    setCookies('email', 'email@email.com', { path: '/' })
+  }, []);
+
   //          render: 헤더 컴포넌트 렌더링          //
   return (
     <div id='header'>
@@ -82,9 +104,9 @@ export default function Header() {
         </div>
         <div className='header-right-box'>
           { isAuthPage  && (<Search />) }
-          { isMainPage && (<></>) }
-          { isSearchPage && (<></>) }
-          { isBoardDetailPage && (<></>) }
+          { isMainPage && (<><Search /> <LoginMyPageButton /></>) }
+          { isSearchPage && (<><Search /> <LoginMyPageButton /></>) }
+          { isBoardDetailPage && (<><Search /> <LoginMyPageButton /></>) }
           { isUserPage && (<></>) }
           { isBoardWritePage && (<></>) }
           { isBoardUpdatePage && (<></>) }
