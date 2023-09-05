63d7af30732584ed6d55c766aa0b12bb03ce0b60
Yang Heejenog <yanghj990@gmeil.com>
2023-09-05 09:52:15 +0900

    2023.09.05 1교시

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import './style.css';
 
+//          component: 메인 페이지          //
 export default function Main() {
+
+  //          component: 메인 상단 컴포넌트          //
+  const MainTop = () => {
+
+    //          render: 메인 상단 컴포넌트 렌더링          //
+    return (
+      <div></div>
+    );
+  }  
+  //          component: 메인 하단 컴포넌트          //
+  const MainBottom = () => {
+
+    //          render: 메인 하단 컴포넌트 렌더링          //
+    return (
+      <div></div>
+    );
+  }
+
+  //          render: 메인 페이지 렌더링          //
   return (
     <div>Main</div>
   )
