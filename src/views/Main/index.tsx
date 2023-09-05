7afd38bcbdfb63c49a4cf0088d65a61ff5de005f
Yang Heejenog <yanghj990@gmeil.com>
2023-09-05 10:51:51 +0900

    2023.09.05 2교시

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
+import { top3ListMock } from 'mocks';
+import { BoardItem } from 'types';
+import Top3ListItem from 'components/Top3Listitem';
 
 //          component: 메인 페이지          //
 export default function Main() {
@@ -7,9 +11,28 @@ export default function Main() {
   //          component: 메인 상단 컴포넌트          //
   const MainTop = () => {
 
+    //          state: 주간 TOP3 게시물 리스트 상태          //
+    const [top3List, setTop3List] = useState<BoardItem[]>([]);
+
+    //          effect: 컴포넌트가 마운트 시 top3 리스트 불러오기          //
+    useEffect(() => {
+      // TODO: API 호출로 변경
+      setTop3List(top3ListMock);
+    }, []);
+
     //          render: 메인 상단 컴포넌트 렌더링          //
     return (
-      <div></div>
+      <div id='main-top-wrapper'>
+        <div className='main-top-container'>
+          <div className='main-top-intro'>{'Hoon Board에서\n다양한 이야기를 나눠보세요'}</div>
+          <div className='main-top-contents-box'>
+            <div className='main-top-contents-title'>{'주간 TOP 3 게시글'}</div>
+            <div className='main-top-contents'>
+              {top3List.map(boardItem => <Top3ListItem boardItem={boardItem} />)}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }  
   //          component: 메인 하단 컴포넌트          //
@@ -17,12 +40,31 @@ export default function Main() {
 
     //          render: 메인 하단 컴포넌트 렌더링          //
     return (
-      <div></div>
+      <div id='main-bottom-wrapper'>
+        <div className='main-bottom-container'>
+          <div className='main-bottom-title'>{'최신 게시물'}</div>
+          <div className='main-bottom-contents-box'>
+            <div className='main-bottom-latest-contents-box'></div>
+            <div className='main-bottom-popular-word-box'>
+              <div className='main-bottom-popular-word-card'>
+                <div className='main-bottom-popular-card-box'>
+                  <div className='main-bottom-popular-card-title'>{'인기 검색어'}</div>
+                  <div className='main-bottom-popular-card-contents'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='main-bottom-pagination-box'></div>
+        </div>
+      </div>
     );
   }
 
   //          render: 메인 페이지 렌더링          //
-  return (
-    <div>Main</div>
+  return ( 
+    <>
+      <MainTop />
+      <MainBottom />
+    </>
   )
 }
