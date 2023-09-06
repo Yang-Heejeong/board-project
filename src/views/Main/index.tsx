e5e5462afe1b48d5e1400df9d6a9d1befec1afa9
Yang Heejenog <yanghj990@gmeil.com>
2023-09-06 12:00:44 +0900

    2023.09.06 3교시

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -7,6 +7,7 @@ import Top3ListItem from 'components/Top3Listitem';
 import { useNavigate } from 'react-router-dom';
 import { SEARCH_PATH } from 'constant';
 import BoardListItem from 'components/BoardListitem';
+import Pagination from 'components/Pagination';
 
 //          component: 메인 페이지          //
 export default function Main() {
@@ -64,29 +65,7 @@ export default function Main() {
     }
 
 
-    //          event handler: 페이지 번호 클릭 이벤트 처리          //
-    const onPageNumberClickHandler = (pageNumber: number) => {
-      setCurrentPageNumber(pageNumber);
-    }
-    //          event handler: 다음 버튼 클릭 이벤트 처리          //
-    const onNextButtonClickHandler = () => {
-      const TOTAL_SECTION = Math.floor((currentBoardListMock.length - 1) / 50) + 1;
-      if (currentSectionNumber === TOTAL_SECTION) {
-        alert('마지막 섹션입니다.');
-        return;
-      }
-      setCurrentPageNumber(currentSectionNumber * 10 + 1);
-      setCurrentSectionNumber(currentSectionNumber + 1);
-    }
-    //          event handler: 이전 버튼 클릭 이벤트 처리          //
-    const onPreviousButtonClickHandler = () => {
-      if (currentSectionNumber === 1) {
-        alert('첫번째 섹션입니다.');
-        return;
-      }
-      setCurrentPageNumber((currentSectionNumber - 1) * 10);
-      setCurrentSectionNumber(currentSectionNumber - 1);
-    }
+    
 
 
 
@@ -134,11 +113,7 @@ export default function Main() {
       setViewPageNumberList(tmpPageNumberList);
 
     } ,[currentSectionNumber]);
-
-
-
-
-
+    
     //          render: 메인 하단 컴포넌트 렌더링          //
     return (
       <div id='main-bottom-wrapper'>
@@ -160,23 +135,14 @@ export default function Main() {
             </div>
           </div>
           <div className='main-bottom-pagination-box'>
-            <div className='pagination-container'>
-              <div className='pagination-change-link-box' onClick={onPreviousButtonClickHandler}>
-                <div className='pagination-change-link-icon-box'>
-                  <div className='left-light-icon'></div>
-                </div>
-                <div className='pagination-change-link-text'>{'이전'}</div>
-              </div>
-              <div className='pagination-divider'>{'\|'}</div>
-              { viewPageNumberList.map(pageNumber => pageNumber === currentPageNumber ? <div className='pagination-active-text'>{pageNumber}</div> : <div className='pagination-text' onClick={() => onPageNumberClickHandler(pageNumber)}>{pageNumber}</div>) }
-              <div className='pagination-divider'>{'\|'}</div>
-              <div className='pagination-change-link-box' onClick={onNextButtonClickHandler}>
-                <div className='pagination-change-link-text'>{'다음'}</div>
-                <div className='pagination-change-link-icon-box'>
-                  <div className='right-light-icon'></div>
-                </div>
-              </div>
-            </div>
+            <Pagination  
+              currentPageNumber={currentPageNumber}
+              currentSectionNumber={currentSectionNumber}
+              setCurrentPageNumber={setCurrentPageNumber}
+              setCurrentSectionNumber={setCurrentSectionNumber}
+              viewPageNumberList={viewPageNumberList}
+              totalSection={0}
+            />
           </div>
         </div>
       </div>
