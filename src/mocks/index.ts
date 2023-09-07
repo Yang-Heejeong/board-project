f620a53916e5134eb8a12caf171c36780b26f7fd
Yang Heejenog <yanghj990@gmeil.com>
2023-09-07 11:02:08 +0900

    2023.09.07 2교시:

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,13 +1,17 @@
-import commentListMock from './comment-list.mock';
-import loginInfoMock from './login-list.mock';
-import currentBoardListMock from './current-board-list.mock';
-import popularWordListMock from './popular-word-list.mock'
 import top3ListMock from './Top3-list.mock';
+import loginInfoMock from './login-list.mock';
+import searchListMock from './search-list-mock';
+import commentListMock from './comment-list.mock';
+import popularWordListMock from './popular-word-list.mock'
+import currentBoardListMock from './current-board-list.mock';
+import relationWordListMock from './relation-word-list.mock';
 
 export {
     top3ListMock,
     loginInfoMock,
-    currentBoardListMock,
-    popularWordListMock,
+    searchListMock,
     commentListMock,
+    popularWordListMock,
+    currentBoardListMock,
+    relationWordListMock,
 };
