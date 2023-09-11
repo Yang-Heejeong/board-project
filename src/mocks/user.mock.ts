9d50462e77e5af9c5905f1a3154e526643bfab34
Yang Heejenog <yanghj990@gmeil.com>
2023-09-11 11:50:06 +0900

    2023.09.11 2,3교시

---
+++ src/mocks/user.mock.ts
@@ -0,0 +1,9 @@
+import { User } from "types";
+
+const userMock: User = {
+    email: 'email@email.com',
+    nickname: '나는주코야키',
+    profileImage: 'https://cdn.pixabay.com/photo/2023/01/06/18/03/wreath-7701834_640.jpg'
+}
+
+export default userMock;
